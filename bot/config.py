fc7da7f7ec007032db0571cc74d7c6e5ccb188a7
allelleo <dev.allelleo@internet.ru>
2024-04-11 13:05:10 +0300

    delete token

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,6 @@
 "Тут мы храним все настройки"
 
-TOKEN = "6858199428:AAE9hdUSDSFFGX08_4IfusBPjD1Hu0xMbY4"
+TOKEN = "6858199428:AAE9hdUSDSFFGX08_"
 ADMIN_IDS = [1137363239]
 db_url = "sqlite://database.db"
 modules = {"models": ["database.models"]}
